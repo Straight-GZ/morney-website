51d1f3404f738246d627a0d5363bba3494dc4180
gengzhii <gengzhi1993@163.com>
2021-03-16 17:38:36 +0800

    deploy

---
+++ precache-manifest.fadff63cdcdf009069e4a5bdf51f8e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55403668b448631c64b5877a9ab8e523",
+    "url": "/morney-website/code.png"
+  },
+  {
+    "revision": "c119a8127c1336d16848",
+    "url": "/morney-website/css/app.47b61f9d.css"
+  },
+  {
+    "revision": "627c7a1abb9021a7326fab249d831c8b",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "c119a8127c1336d16848",
+    "url": "/morney-website/js/app.4748a2c5.js"
+  },
+  {
+    "revision": "73ca8054732acba31270",
+    "url": "/morney-website/js/chunk-vendors.14da9975.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
